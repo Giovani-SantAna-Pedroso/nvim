73e2b1170990fd338a70e83a0c998b4391d9c04b
Giovani Sant'Ana Ferreira Pedroso Vaz <giovanisfpv@gmail.com>
2024-03-31 16:22:12 -0300

    UFO plugin added

--- lua/giovani/folding.lua
+++ lua/giovani/folding.lua
@@ -1,5 +1,15 @@
 local vim = vim
 local opt = vim.opt
 
+-- zc closes the fold
+-- zo opens the fold
+-- za toggles the fold
+
 opt.foldmethod = "expr"
 opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.foldlevelstart = 99
+opt.nofoldenable = true
+
+local a = {
+	atr,
+}
--- lua/giovani/plugins/init.lua
+++ lua/giovani/plugins/init.lua
@@ -1,6 +1,7 @@
 return {
 	"norcalli/nvim-colorizer.lua",
 	"lewis6991/gitsigns.nvim",
+	"kevinhwang91/promise-async",
 	"phaazon/hop.nvim",
 	{ "folke/todo-comments.nvim", dependecies = {
 		"nvim-lua/plenary.nvim",
