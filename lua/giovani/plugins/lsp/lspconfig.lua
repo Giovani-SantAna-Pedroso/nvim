8eb4e0e76413635aa94f1953ab88d87d0f3e4bfa
Giovani Sant'Ana <giovanisfpv@gmail.com>
2024-08-17 17:35:38 -0300

    test cellphone

--- lua/giovani/plugins/lsp/lspconfig.lua
+++ lua/giovani/plugins/lsp/lspconfig.lua
@@ -15,67 +15,17 @@ return {
 			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 		end
 
+		-- This function will tell if the nvim is runing on a pc or a cellphone
+		local function get_architecture()
+			local pipe = io.popen("uname -m") -- for Unix-like systems
+			local architecture = pipe:read("*a")
+			pipe:close()
+			return architecture:gsub("%s+", "") -- remove any trailing whitespace
+		end
+
+		local architcture = get_architecture()
+
 		lspconfig["bashls"].setup({
-			capabilities = basic.capabilities,
-			on_attach = basic.on_attach,
-		})
-
-		lspconfig["clangd"].setup({
-			capabilities = basic.capabilities,
-			on_attach = basic.on_attach,
-			settings = require("giovani.plugins.lsp.settings.clangd"),
-		})
-
-		lspconfig["cssls"].setup({
-			capabilities = basic.capabilities,
-			on_attach = basic.on_attach,
-		})
-
-		lspconfig["docker_compose_language_service"].setup({
-			capabilities = basic.capabilities,
-			on_attach = basic.on_attach,
-		})
-
-		lspconfig["dockerls"].setup({
-			capabilities = basic.capabilities,
-			on_attach = basic.on_attach,
-		})
-
-		lspconfig["html"].setup({
-			capabilities = basic.capabilities,
-			on_attach = basic.on_attach,
-		})
-
-		lspconfig["jsonls"].setup({
-			capabilities = basic.capabilities,
-			on_attach = basic.on_attach,
-			-- settings =  require("giovani.plugins.lsp.settings.json")
-		})
-
-		lspconfig["lua_ls"].setup({
-			capabilities = basic.capabilities,
-			on_attach = basic.on_attach,
-			settings = require("giovani.plugins.lsp.settings.lua"),
-		})
-
-		-- ✓ csharp-language-server csharp_ls
-
-		lspconfig["csharp_ls"].setup({
-			capabilities = basic.capabilities,
-			on_attach = basic.on_attach,
-		})
-		-- lspconfig["omnisharp"].setup({
-		-- 	capabilities = basic.capabilities,
-		-- 	on_attach = basic.on_attach,
-		-- 	cmd = { "dotnet", "/usr/bin/omnisharp" },
-		-- enable_roslyn_analysers = true,
-		-- enable_import_completion = true,
-		-- organize_imports_on_format = true,
-		-- enable_decompilation_support = true,
-		-- settings = require("giovani.plugins.lsp.settings.omnisharp"),
-		-- })
-
-		lspconfig["prismals"].setup({
 			capabilities = basic.capabilities,
 			on_attach = basic.on_attach,
 		})
@@ -86,38 +36,101 @@ return {
 			settings = require("giovani.plugins.lsp.settings.pyright"),
 		})
 
-		lspconfig["rust_analyzer"].setup({
-			capabilities = basic.capabilities,
-			on_attach = basic.on_attach,
-			settings = require("giovani.plugins.lsp.settings.rust_analyzer"),
-		})
-
-		lspconfig["tailwindcss"].setup({
-			capabilities = basic.capabilities,
-			on_attach = basic.on_attach,
-			settings = require("giovani.plugins.lsp.settings.tsserver"),
-		})
-
 		lspconfig["tsserver"].setup({
 			capabilities = basic.capabilities,
 			on_attach = basic.on_attach,
 			settings = require("giovani.plugins.lsp.settings.tsserver"),
 		})
 
-		lspconfig["svelte"].setup({
-			capabilities = basic.capabilities,
-			on_attach = basic.on_attach,
-			settings = require("giovani.plugins.lsp.settings.svelte"),
-		})
+		-- Only install this if the nvim is runing on a PC
+		if string.find(architcture, "x86_64") ~= nil then
+			lspconfig["clangd"].setup({
+				capabilities = basic.capabilities,
+				on_attach = basic.on_attach,
+				settings = require("giovani.plugins.lsp.settings.clangd"),
+			})
 
-		lspconfig["yamlls"].setup({
-			capabilities = basic.capabilities,
-			on_attach = basic.on_attach,
-		})
+			lspconfig["cssls"].setup({
+				capabilities = basic.capabilities,
+				on_attach = basic.on_attach,
+			})
 
-		lspconfig["solidity_ls"].setup({
-			capabilities = basic.capabilities,
-			on_attach = basic.on_attach,
-		})
+			lspconfig["docker_compose_language_service"].setup({
+				capabilities = basic.capabilities,
+				on_attach = basic.on_attach,
+			})
+
+			lspconfig["dockerls"].setup({
+				capabilities = basic.capabilities,
+				on_attach = basic.on_attach,
+			})
+
+			lspconfig["html"].setup({
+				capabilities = basic.capabilities,
+				on_attach = basic.on_attach,
+			})
+
+			lspconfig["jsonls"].setup({
+				capabilities = basic.capabilities,
+				on_attach = basic.on_attach,
+				-- settings =  require("giovani.plugins.lsp.settings.json")
+			})
+
+			lspconfig["lua_ls"].setup({
+				capabilities = basic.capabilities,
+				on_attach = basic.on_attach,
+				settings = require("giovani.plugins.lsp.settings.lua"),
+			})
+
+			-- ✓ csharp-language-server csharp_ls
+
+			lspconfig["csharp_ls"].setup({
+				capabilities = basic.capabilities,
+				on_attach = basic.on_attach,
+			})
+			-- lspconfig["omnisharp"].setup({
+			-- 	capabilities = basic.capabilities,
+			-- 	on_attach = basic.on_attach,
+			-- 	cmd = { "dotnet", "/usr/bin/omnisharp" },
+			-- enable_roslyn_analysers = true,
+			-- enable_import_completion = true,
+			-- organize_imports_on_format = true,
+			-- enable_decompilation_support = true,
+			-- settings = require("giovani.plugins.lsp.settings.omnisharp"),
+			-- })
+
+			lspconfig["prismals"].setup({
+				capabilities = basic.capabilities,
+				on_attach = basic.on_attach,
+			})
+
+			lspconfig["rust_analyzer"].setup({
+				capabilities = basic.capabilities,
+				on_attach = basic.on_attach,
+				settings = require("giovani.plugins.lsp.settings.rust_analyzer"),
+			})
+
+			lspconfig["tailwindcss"].setup({
+				capabilities = basic.capabilities,
+				on_attach = basic.on_attach,
+				settings = require("giovani.plugins.lsp.settings.tsserver"),
+			})
+
+			lspconfig["svelte"].setup({
+				capabilities = basic.capabilities,
+				on_attach = basic.on_attach,
+				settings = require("giovani.plugins.lsp.settings.svelte"),
+			})
+
+			lspconfig["yamlls"].setup({
+				capabilities = basic.capabilities,
+				on_attach = basic.on_attach,
+			})
+
+			lspconfig["solidity_ls"].setup({
+				capabilities = basic.capabilities,
+				on_attach = basic.on_attach,
+			})
+		end
 	end,
 }
--- lua/giovani/plugins/lsp/settings/basic.lua
+++ lua/giovani/plugins/lsp/settings/basic.lua
@@ -12,43 +12,43 @@ local on_attach = function(client, bufnr)
 
 	-- set keybinds
 	opts.desc = "Show LSP references"
-	keymap.set("n", "çgr", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+	keymap.set("n", "<leader>qgr", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
 
 	opts.desc = "Go to declaration"
-	keymap.set("n", "<leader>çgd", vim.lsp.buf.declaration, opts) -- go to declaration
+	keymap.set("n", "<leader>qgdc", vim.lsp.buf.declaration, opts) -- go to declaration
 
 	opts.desc = "Show LSP definitions"
-	keymap.set("n", "<leader>çgd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+	keymap.set("n", "<leader>qgdc", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
 
 	opts.desc = "Show LSP implementations"
-	keymap.set("n", "<leader>çgi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+	keymap.set("n", "<leader>qgi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
 
 	opts.desc = "Show LSP type definitions"
-	keymap.set("n", "<leader>çgt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+	keymap.set("n", "<leader>qgt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
 
 	opts.desc = "See available code actions"
-	keymap.set({ "n", "<leader>ça" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+	keymap.set({ "n", "<leader>qa" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
 	opts.desc = "Smart rename"
-	keymap.set("n", "<leader>çrn", vim.lsp.buf.rename, opts) -- smart rename
+	keymap.set("n", "<leader>qrn", vim.lsp.buf.rename, opts) -- smart rename
 
 	opts.desc = "Show buffer diagnostics"
-	keymap.set("n", "<leader>çdb", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+	keymap.set("n", "<leader>qdb", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
 
 	opts.desc = "Show line diagnostics"
-	keymap.set("n", "<leader>çdl", vim.diagnostic.open_float, opts) -- show diagnostics for line
+	keymap.set("n", "<leader>qdl", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
 	opts.desc = "Go to previous diagnostic"
-	keymap.set("n", "<leader>çm", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+	keymap.set("n", "<leader>qm", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
 
 	opts.desc = "Go to next diagnostic"
-	keymap.set("n", "<leader>çdi", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+	keymap.set("n", "<leader>qdi", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
 
 	opts.desc = "Show documentation for what is under cursor"
-	keymap.set("n", "<leader>çk", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+	keymap.set("n", "<leader>qk", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
 	opts.desc = "Restart LSP"
-	keymap.set("n", "<leader>çr", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+	keymap.set("n", "<leader>qr", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
 end
 
 M.on_attach = on_attach
