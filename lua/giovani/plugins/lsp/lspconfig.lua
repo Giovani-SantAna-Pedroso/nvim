f92644b4a628849895fdc0d2e0cf3c47dc5a0dc4
Giovani Sant'Ana <giovanisfpv@gmail.com>
2024-08-17 17:45:07 -0300

    test cellphone

--- lua/giovani/plugins/lsp/lspconfig.lua
+++ lua/giovani/plugins/lsp/lspconfig.lua
@@ -42,38 +42,83 @@ return {
 			settings = require("giovani.plugins.lsp.settings.tsserver"),
 		})
 
+		lspconfig["cssls"].setup({
+			capabilities = basic.capabilities,
+			on_attach = basic.on_attach,
+		})
+
+		lspconfig["docker_compose_language_service"].setup({
+			capabilities = basic.capabilities,
+			on_attach = basic.on_attach,
+		})
+
+		lspconfig["dockerls"].setup({
+			capabilities = basic.capabilities,
+			on_attach = basic.on_attach,
+		})
+
+		lspconfig["html"].setup({
+			capabilities = basic.capabilities,
+			on_attach = basic.on_attach,
+		})
+
+		lspconfig["jsonls"].setup({
+			capabilities = basic.capabilities,
+			on_attach = basic.on_attach,
+			-- settings =  require("giovani.plugins.lsp.settings.json")
+		})
+
+		-- ✓ csharp-language-server csharp_ls
+
+		-- lspconfig["omnisharp"].setup({
+		-- 	capabilities = basic.capabilities,
+		-- 	on_attach = basic.on_attach,
+		-- 	cmd = { "dotnet", "/usr/bin/omnisharp" },
+		-- enable_roslyn_analysers = true,
+		-- enable_import_completion = true,
+		-- organize_imports_on_format = true,
+		-- enable_decompilation_support = true,
+		-- settings = require("giovani.plugins.lsp.settings.omnisharp"),
+		-- })
+
+		lspconfig["prismals"].setup({
+			capabilities = basic.capabilities,
+			on_attach = basic.on_attach,
+		})
+
+		lspconfig["tailwindcss"].setup({
+			capabilities = basic.capabilities,
+			on_attach = basic.on_attach,
+			settings = require("giovani.plugins.lsp.settings.tsserver"),
+		})
+
+		lspconfig["svelte"].setup({
+			capabilities = basic.capabilities,
+			on_attach = basic.on_attach,
+			settings = require("giovani.plugins.lsp.settings.svelte"),
+		})
+
+		lspconfig["yamlls"].setup({
+			capabilities = basic.capabilities,
+			on_attach = basic.on_attach,
+		})
+
+		lspconfig["solidity_ls"].setup({
+			capabilities = basic.capabilities,
+			on_attach = basic.on_attach,
+		})
+
 		-- Only install this if the nvim is runing on a PC
 		if string.find(architcture, "x86_64") ~= nil then
+			lspconfig["csharp_ls"].setup({
+				capabilities = basic.capabilities,
+				on_attach = basic.on_attach,
+			})
+
 			lspconfig["clangd"].setup({
 				capabilities = basic.capabilities,
 				on_attach = basic.on_attach,
 				settings = require("giovani.plugins.lsp.settings.clangd"),
-			})
-
-			lspconfig["cssls"].setup({
-				capabilities = basic.capabilities,
-				on_attach = basic.on_attach,
-			})
-
-			lspconfig["docker_compose_language_service"].setup({
-				capabilities = basic.capabilities,
-				on_attach = basic.on_attach,
-			})
-
-			lspconfig["dockerls"].setup({
-				capabilities = basic.capabilities,
-				on_attach = basic.on_attach,
-			})
-
-			lspconfig["html"].setup({
-				capabilities = basic.capabilities,
-				on_attach = basic.on_attach,
-			})
-
-			lspconfig["jsonls"].setup({
-				capabilities = basic.capabilities,
-				on_attach = basic.on_attach,
-				-- settings =  require("giovani.plugins.lsp.settings.json")
 			})
 
 			lspconfig["lua_ls"].setup({
@@ -82,54 +127,10 @@ return {
 				settings = require("giovani.plugins.lsp.settings.lua"),
 			})
 
-			-- ✓ csharp-language-server csharp_ls
-
-			lspconfig["csharp_ls"].setup({
-				capabilities = basic.capabilities,
-				on_attach = basic.on_attach,
-			})
-			-- lspconfig["omnisharp"].setup({
-			-- 	capabilities = basic.capabilities,
-			-- 	on_attach = basic.on_attach,
-			-- 	cmd = { "dotnet", "/usr/bin/omnisharp" },
-			-- enable_roslyn_analysers = true,
-			-- enable_import_completion = true,
-			-- organize_imports_on_format = true,
-			-- enable_decompilation_support = true,
-			-- settings = require("giovani.plugins.lsp.settings.omnisharp"),
-			-- })
-
-			lspconfig["prismals"].setup({
-				capabilities = basic.capabilities,
-				on_attach = basic.on_attach,
-			})
-
 			lspconfig["rust_analyzer"].setup({
 				capabilities = basic.capabilities,
 				on_attach = basic.on_attach,
 				settings = require("giovani.plugins.lsp.settings.rust_analyzer"),
-			})
-
-			lspconfig["tailwindcss"].setup({
-				capabilities = basic.capabilities,
-				on_attach = basic.on_attach,
-				settings = require("giovani.plugins.lsp.settings.tsserver"),
-			})
-
-			lspconfig["svelte"].setup({
-				capabilities = basic.capabilities,
-				on_attach = basic.on_attach,
-				settings = require("giovani.plugins.lsp.settings.svelte"),
-			})
-
-			lspconfig["yamlls"].setup({
-				capabilities = basic.capabilities,
-				on_attach = basic.on_attach,
-			})
-
-			lspconfig["solidity_ls"].setup({
-				capabilities = basic.capabilities,
-				on_attach = basic.on_attach,
 			})
 		end
 	end,
--- lua/giovani/plugins/lsp/mason.lua
+++ lua/giovani/plugins/lsp/mason.lua
@@ -21,9 +21,20 @@ return {
 			},
 		})
 
-		-- import mason-lspconfig
-		mason_lspconfig.setup({
-			ensure_installed = {
+		local toInstallLsp = {}
+		local toInstallNull = {}
+
+		local function get_architecture()
+			local pipe = io.popen("uname -m") -- for Unix-like systems
+			local architecture = pipe:read("*a")
+			pipe:close()
+			return architecture:gsub("%s+", "") -- remove any trailing whitespace
+		end
+
+		local architcture = get_architecture()
+
+		if string.find(architcture, "x86_64") ~= nil then
+			toInstallLsp = {
 				"bashls",
 				"lua_ls",
 				"clangd",
@@ -42,18 +53,48 @@ return {
 				-- "solidity_ls",
 				"cssls",
 				-- "yaml-language-server",
-			},
-			automatic_installation = true,
-		})
+			}
 
-		mason_null.setup({
-			ensure_installed = {
+			toInstallNull = {
 				"prettier",
 				"stylua",
 				"rustfmt",
 				"eslint_d",
 				"black",
-			},
+			}
+		else
+			toInstallLsp = {
+				"bashls",
+				"dockerls",
+				"docker_compose_language_service",
+				"html",
+				"pyright",
+				"tsserver",
+				"jsonls",
+				"svelte",
+				"tailwindcss",
+				"volar",
+				-- "vscode-solidity-server",
+				-- "solidity_ls",
+				"cssls",
+				-- "yaml-language-server",
+			}
+
+			toInstallNull = {
+				"prettier",
+				"eslint_d",
+				"black",
+			}
+		end
+
+		-- import mason-lspconfig
+		mason_lspconfig.setup({
+			ensure_installed = toInstallLsp,
+			automatic_installation = true,
+		})
+
+		mason_null.setup({
+			ensure_installed = toInstallNull,
 			automatic_installation = true,
 		})
 	end,
