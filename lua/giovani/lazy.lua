295a08b42fddec01b5aa837edcb9f05a070d08ff
Giovani Sant'Ana Pedroso <giovanisfpv@gmail.com>
2025-10-27 00:59:25 -0300

    add moltem and image

--- lua/giovani/lazy.lua
+++ lua/giovani/lazy.lua
@@ -21,6 +21,7 @@ require("lazy").setup({
 	{ import = "giovani.plugins.useless" },
 	{ import = "giovani.plugins.editing" },
 	{ import = "giovani.plugins.dap" },
+	{ import = "giovani.plugins.jupyter" },
 	checker = { enable = true, notify = false },
 	change_detection = { notify = false },
 })
